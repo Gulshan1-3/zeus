db3eba3032a1946064a7fa72e2d032ca43650947
Gulshan1-3 <ankitstar955@gmail.com>
2025-02-05 17:06:12 +0530

    ok

---
+++ editor/target/debug/deps/editor-c1689f6e1cd7cef9.d
@@ -0,0 +1,5 @@
+/home/gulshansharma/zeus/editor/target/debug/deps/editor-c1689f6e1cd7cef9: src/main.rs
+
+/home/gulshansharma/zeus/editor/target/debug/deps/editor-c1689f6e1cd7cef9.d: src/main.rs
+
+src/main.rs:
